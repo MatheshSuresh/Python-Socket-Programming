facc65c4045c034c3798028bf918689b5f5f9343
Mathesh Suresh <33441626+MatheshSuresh@users.noreply.github.com>
2019-11-14 20:48:28 +0530

    Add files via upload

---
+++ Socket Programming/Buffering/server.py
@@ -0,0 +1,17 @@
+import socket
+
+HEADERSIZE = 10
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((socket.gethostname(), 1241))
+s.listen(5)
+
+while True:
+    # now our endpoint knows about the OTHER endpoint.
+    clientsocket, address = s.accept()
+    print(f"Connection from {address} has been established.")
+
+    msg = "Welcome to the server!"
+    msg = f"{len(msg):<{HEADERSIZE}}"+msg
+
+    clientsocket.send(bytes(msg,"utf-8"))
